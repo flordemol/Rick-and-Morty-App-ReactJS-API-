e628ab379865f241a13585f66491da2160b373da
Maria Florencia De Mollein <florenciademollein@gmail.com>
2021-01-12 16:03:58 -0300

    Capa presentación - Estructura Character

--- src/Components/Characters/Character.js
+++ src/Components/Characters/Character.js
@@ -1,8 +1,28 @@
+import { Col, Card, Button } from "react-bootstrap";
+
 // Componente de presentación
-const Character = () => {
+const Character = ({ id, name, gender, status, species, created, image }) => {
     return ( 
-        <h3>Personaje</h3>
+        <Col md={4} className="my-3 text-center">
+            <Card>
+                <Card.Img variant={"top"} src={image} />
+                <Card.Body>
+                    <Card.Title>
+                        {name} - {gender}
+                    </Card.Title>
+                    <Card.Text>
+                        {status === "Alive" ? "Vivo" : "Muerto"} - {species}
+                    </Card.Text>
+                    <Card.Text>
+                        <small>{created}</small>
+                    </Card.Text>
+                    <Button type="button" variant="primary" style={{ width: '100%' }}>
+                        Ver más
+                    </Button>
+                </Card.Body>
+            </Card>
+        </Col>
      );
-}
+};
  
 export default Character;
